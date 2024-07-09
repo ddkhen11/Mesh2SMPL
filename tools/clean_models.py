51141aae68ceb58669ff5be98ad159d51689b5e0
ddkhen11 <d.d.khen@wustl.edu>
2024-07-09 16:44:57 -0400

    Update model cleaning script

--- tools/clean_models.py
+++ tools/clean_models.py
@@ -19,7 +19,14 @@ def clean_fn(fn, output_folder='output'):
         else:
             output_dict[key] = data
 
-    out_fn = osp.split(fn)[1]
+    if osp.split(fn)[1] == "basicModel_neutral_lbs_10_207_0_v1.0.0.pkl":
+        out_fn = "SMPL_NEUTRAL.pkl"
+    elif osp.split(fn)[1] == "basicModel_m_lbs_10_207_0_v1.0.0.pkl":
+        out_fn = "SMPL_MALE.pkl"
+    elif osp.split(fn)[1] == "basicModel_f_lbs_10_207_0_v1.0.0.pkl":
+        out_fn = "SMPL_FEMALE.pkl"
+    else:
+        raise ValueError('Unknown model: {}'.format(fn))
 
     out_path = osp.join(output_folder, out_fn)
     with open(out_path, 'wb') as out_file:
@@ -30,7 +37,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input-models', dest='input_models', nargs='+',
                         required=True, type=str,
-                        help='The path to the model that will be processed')
+                        help='The path to the models that will be processed')
     parser.add_argument('--output-folder', dest='output_folder',
                         required=True, type=str,
                         help='The path to the output folder')
